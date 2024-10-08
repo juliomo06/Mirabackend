f36062a58f038c42f44e7c587533f39fb2e50122
Julio Angelo R Lim <jrlim@ccc.edu.ph>
2024-10-08 19:46:54 +0800

    update

--- server.js
+++ server.js
@@ -11,16 +11,21 @@ const multer = require('multer');
 const jwt = require('jsonwebtoken');
 const User = require ('./models/User');
 const adminRoutes = require ('./routes/admin')
-
+require ('dotenv').config()
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb+srv://jrlim:pogiako_703@mira.ngwes.mongodb.net/?retryWrites=true&w=majority&appName=mira', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error.message);
 });
+
 
 // USER ROUTES
 app.use('/api/users', userRoutes);
@@ -132,7 +137,8 @@ app.use('/gcash', express.static(path.join(__dirname, 'gcash')));
 // Booking confirmation
 app.use('/api/bookings', bookingRoutes);
 
-
-app.listen(8080, () => {
-  console.log('Server running on port 8080');
+//start
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server is running on port` + PORT);
 });
